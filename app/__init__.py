6a7fec16419d2b6576b09eb09c793ecb3dc58836
Gavin Waite <gavwaite@gmail.com>
2020-01-16 17:37:45 -0500

    Copied over work on Phase 1 of tutorial

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+# Gavin Waite 2020
+# This file creates the Flask app object 
+
+
+import os
+import json
+import datetime
+from flask import Flask
+
+# create the flask object
+app = Flask(__name__)
